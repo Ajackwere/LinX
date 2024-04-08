3cf7ab986e2b2d94c5a9c109facb26c66e54ff43
Sam .M <you@example.com>
2024-04-08 18:28:27 +0300

    changed category layout

--- client/src/Components/Publick/Nav.jsx
+++ client/src/Components/Publick/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
 import "../../Styles/publick/nav.css";
 import { NavLink } from "react-router-dom";
 import { CONT } from "../../context/AppContext";
@@ -7,6 +7,26 @@ import logo from "../../assets/link-logo.png";
 function Nav() {
   const vl = useContext(CONT);
   const [activeCt, setActiveCt] = useState("All");
+  const [showLeftButton, setShowLeftButton] = useState(false);
+  const [showRightButton, setShowRightButton] = useState(false);
+  const navCategoryRef = useRef(null);
+
+  useEffect(() => {
+    const container = navCategoryRef.current;
+    setShowLeftButton(container.scrollLeft > 0);
+    setShowRightButton(
+      container.scrollLeft < container.scrollWidth - container.clientWidth
+    );
+  }, []);
+
+  const handleScroll = () => {
+    const container = navCategoryRef.current;
+    setShowLeftButton(container.scrollLeft > 0);
+    setShowRightButton(
+      container.scrollLeft < container.scrollWidth - container.clientWidth
+    );
+  };
+
   const categories = [
     "All",
     "Sports",
@@ -15,59 +35,98 @@ function Nav() {
     "Business",
     "Technology",
     "Travel",
+    "Health & lifestyle",
+    "Beauty & Skincare",
+    "Business",
+    "Technology",
+    "Travel",
   ];
+
   return (
     <nav>
-      <div className="nav-logo">
-        <img src={logo} alt="" />
-      </div>
-      <ul>
-        <NavLink to="/">
+      <div className="nav-top">
+        <div className="nav-logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="nav-center">
+          <NavLink to="/">
+            <li>
+              <span className="material-symbols-outlined">home</span>
+              <div className="active-line"></div>
+            </li>
+          </NavLink>
           <li>
-            <span className="material-symbols-outlined">home</span>
+            <span className="material-symbols-outlined">bookmark</span>
             <div className="active-line"></div>
           </li>
-        </NavLink>
-        <li>
-          <span className="material-symbols-outlined">bookmark</span>
-          <div className="active-line"></div>
-        </li>
 
-        <li>
-          <span className="material-symbols-outlined">notifications</span>
-          <div className="active-line"></div>
-        </li>
-        <li>
-          <div className="nav-categories">
-            <div className="nav-active-category">
-              <span>{activeCt}</span>{" "}
-              <span className="material-symbols-outlined">expand_more</span>
+          <li>
+            <span className="material-symbols-outlined">notifications</span>
+            <div className="active-line"></div>
+          </li>
+          <li className="nav-search-li">
+            <div className="nav-search">
+              <span className="material-symbols-outlined">search</span>
+              <input
+                type="search"
+                className="nav-search-input"
+                placeholder="Search something"
+              />
             </div>
-            <ul>
-              {categories.map((category) =>
-                category !== activeCt ? (
-                  <li onClick={() => setActiveCt(category)}>{category}</li>
-                ) : null
-              )}
-            </ul>
+          </li>
+          <li>
+            <div className="nav-profile nav-profile2">
+              <img src="" alt="" onError={vl.errorProfileImg} />
+            </div>
+          </li>
+        </ul>
+        <div className="nav-profile">
+          <img src="" alt="" onError={vl.errorProfileImg} />
+        </div>
+
+        <div className="nav-search2">
+          <span className="material-symbols-outlined">search</span>
+        </div>
+      </div>
+      <div className="nav-bottom">
+        {showLeftButton && (
+          <div className="n-c-slider ncs-left" onClick={scrollLeft}>
+            <div className="n-c-slider-btn">
+              <span className="material-symbols-outlined">chevron_left</span>
+            </div>
           </div>
-        </li>
-        <li>
-          <div className="nav-search">
-            <span className="material-symbols-outlined">search</span>
-            <input
-              type="search"
-              className="nav-search-input"
-              placeholder="Search something"
-            />
+        )}
+        {showRightButton && (
+          <div className="n-c-slider ncs-right" onClick={scrollRight}>
+            <div className="n-c-slider-btn">
+              <span className="material-symbols-outlined">chevron_right</span>
+            </div>
           </div>
-        </li>
-      </ul>
-      <div className="nav-profile">
-        <img src="" alt="" onError={vl.errorProfileImg} />
+        )}
+        <div
+          className="nav-categoriy-cnt"
+          onScroll={handleScroll}
+          ref={navCategoryRef}
+        >
+          <ul className="nav-categories">
+            {categories.map((category) => (
+              <li key={category}>{category}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
+
+  function scrollLeft() {
+    const container = navCategoryRef.current;
+    container.scrollLeft -= 100; // Adjust the scroll amount as needed
+  }
+
+  function scrollRight() {
+    const container = navCategoryRef.current;
+    container.scrollLeft += 100; // Adjust the scroll amount as needed
+  }
 }
 
 export default Nav;
